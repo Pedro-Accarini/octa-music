1d46ee50b8d7ff587fdea4543b83105ca0cc6b42
Pedro Accarini Barros de Azevedo <122038856+P4dr4@users.noreply.github.com>
2025-04-20 20:44:05 -0300

    Implement top 5 tracks feature and update search results page

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ CLIENT_SECRET = ''
 auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
+TOP_50_GLOBAL_PLAYLIST_ID = '37i9dQZEVXbMDoHDwVN2tF'
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     artist = None
@@ -25,7 +27,21 @@ def home():
                 'genres': ', '.join(a['genres']),
                 'image_url': a['images'][0]['url'] if a['images'] else None,
             }
-    return render_template("index.html", artist=artist)
+    return render_template("search_pop.html", artist=artist)
+
+@app.route("/top5")
+def top5():
+    top_tracks = []
+    playlist = sp.playlist_items(TOP_50_GLOBAL_PLAYLIST_ID, limit=5)
+    for item in playlist['items']:
+        track = item['track']
+        top_tracks.append({
+            'name': track['name'],
+            'artist': track['artists'][0]['name'],
+            'image_url': track['album']['images'][0]['url'] if track['album']['images'] else None,
+            'preview_url': track['preview_url'],
+        })
+    return render_template("top5.html", top_tracks=top_tracks)
 
 if __name__ == "__main__":
     app.run(debug=True)
